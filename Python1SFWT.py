c2ce2055473076b4de3cf8285fbb3491709a823d
phi <ada_hack245@hackademy.mx>
2022-07-08 08:45:02 -0500

    Evidencia1

---
+++ Python1SFWT.py
@@ -0,0 +1,20 @@
+#define la función "leer número" 
+#define la función "imprime suma"
+
+#lee y guarda primer número
+#lee y guarda segundo número
+#imprime la suma de ambos números
+
+def lee_numero():
+    x = int(input("Ingresa el número: "))
+    return x
+
+def imprime_resultado(x, y):
+    resultado = x + y
+    print("La suma de los números es" , resultado)
+
+def main():
+    x = lee_numero()
+    y = lee_numero() 
+    imprime_resultado(x, y) 
+main()
